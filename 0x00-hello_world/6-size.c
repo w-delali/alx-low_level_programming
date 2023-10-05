59cccddb2b6e6a2e8c4a00016cd8e558aa73c62d
delali <brightdelaliwordi@gmail.com>
2023-10-05 16:43:32 +0000

    updated task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
+
 /**
- * main - stating piont of program
- *  
- * Return: 0
- *            
+ *main - starting point of program
+ *Return: 0
  */
 int main(void)
 {
-		char c;
-		int i;
-		float f;
-	printf("Size of a char: %lu byte\(s\)\n", sizeof(c));
-	printf("Size of an int: %lu byte\(s\)\n", sizeof(i));
-        printf("Size of a long int: %lu byte\(s\)\n", sizeof(long int));
-        printf("Size of a long long int: %lu byte\(s\)\n", sizeof(long long int));
-	printf("Size of a float: %lu byte\(s\)\n", sizeof(f));
-	return (0);
+		printf("Size of a char: %d byte(s)\n", sizeof(char));
+			printf("Size of an int: %d byte(s)\n", sizeof(int));
+				printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+					printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+						printf("Size of a float: %d byte(s)\n", sizeof(float));
+							return (0);
 }
