06b323a2069e87e6674340c4c4b97992568c9f5b
delali <brightdelaliwordi@gmail.com>
2023-10-10 19:46:04 +0000

    applied update

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,7 +14,7 @@ int main(void)
 	unsigned long sum;
 	unsigned long fib1_half1;
 	unsigned long fib1_half2;
-	unsigned long fib2_half1,
+	unsigned long fib2_half1;
 	unsigned long fib2_half2;
 	unsigned long half1;
 	unsigned long half2;
