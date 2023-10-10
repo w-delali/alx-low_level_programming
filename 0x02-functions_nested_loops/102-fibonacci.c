7e4756922f70afb7fd272a20d1730bedce6914fb
delali <brightdelaliwordi@gmail.com>
2023-10-10 19:16:50 +0000

    added task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints first 50 Fibonacci numbers, starting with 1 and 2
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int count;
+	unsigned long fib1 = 0, 
+	unsigned long fib2 = 1, 
+	unsigned long sum;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -43,3 +43,6 @@
 
 ### 101-natural
 - a program that computes and prints the sum of all the multiples of 3 or 5 below 1024
+
+### 102-fibonacci
+- a program that prints the first 50 Fibonacci numbers, starting with 1 and 2
