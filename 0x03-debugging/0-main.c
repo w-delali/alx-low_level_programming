1c6d3ee6ba6f8172f4725bb1b9a70989a9ab814e
delali <brightdelaliwordi@gmail.com>
2023-10-11 09:01:26 +0000

    added task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - checks if a number is positive or negative from a given function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	num = 0;
+	positive_or_negative(num);
+
+	return(0);
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,6 @@
+
+# FUNCTIONS OF CODE
+---
+
+### 0-main
+- a script to test a function and gives an output
