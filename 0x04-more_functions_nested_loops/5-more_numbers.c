93b8180ae44cdb1fbcfa522f0d17138815062415
delali <brightdelaliwordi@gmail.com>
2023-10-13 01:54:13 +0000

    added task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,17 +4,16 @@
  * more_numbers - print 0 to 14 ten times
  * Return: void
  */
+
 void more_numbers(void)
 {
-	int a, ch;
+	int a, b;
 
 	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b < 15; b++)
+		for (b = 0; b <= 14; b++)
 		{
-			if (b >= 10)
-				_putchar((b / 10) + 48);
-			_putchar((b % 10) + 48);
+			_putchar((b % 10) + '0');
 		}
 		_putchar('\n');
 	}
