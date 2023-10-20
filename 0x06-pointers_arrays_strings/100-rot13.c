6b98cdc698a612af9444f9ef72a868d5398e238c
delali <brightdelaliwordi@gmail.com>
2023-10-20 02:20:10 +0000

    added task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - encoding string using rot13.
+ * @s: string to be encoded
+ * Return: encoded string
+ */
+
+char *rot13(char *s)
+{
+	int i = 0, j = 0;
+	char string_rot13[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
+	char string_alpha[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+
+	while (s[i] != '\0')
+	{
+		do {
+			if (s[i] == string_alpha[j])
+			{
+				s[i] = string_rot13[j];
+				break;
+			}
+			j++;
+		} while (string_alpha[j] != '\0');
+		j = 0;
+		i++;
+	}
+
+	return (s);
+}
