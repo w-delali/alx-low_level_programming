20aaf5f11445a7334a9e690c178dc0cd1b22f9f0
delali <brightdelaliwordi@gmail.com>
2023-10-27 23:59:26 +0000

    added task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - starting point
+ *
+ * @argc: counts the number of parameters that goes into main
+ * @argv: pointer of array of pointers 
+ * containing strings in main param
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
